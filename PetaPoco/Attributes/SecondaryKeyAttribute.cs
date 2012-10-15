6301c8899eab5a83a3080ee79a2b0da428dce12b
Michael Pakhantsov <michael.pakhantsov@gmail.com>
2012-10-15 09:05:59 +0300

    crlf problem

---
+++ PetaPoco/Attributes/SecondaryKeyAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PetaPoco
+{
+
+
+    /// <summary>
+    /// Specifies the secondary key column of a poco class, whether the column is auto incrementing
+    /// and the sequence name for Oracle sequence columns.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SecondaryKeyAttribute : Attribute
+    {
+        public SecondaryKeyAttribute(string key)
+        {
+            this.Value = key;
+        }
+
+        public string Value
+        {
+            get;
+            private set;
+        }
+    }
+}
